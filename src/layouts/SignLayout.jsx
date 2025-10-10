6f18c79d387621fdf57d2f78477fbf451229cc55
soztrk <s.ozturk999@gmail.com>
2025-10-10 19:57:07 +0300

    Sign layout grid fix

--- src/layouts/SignLayout.jsx
+++ src/layouts/SignLayout.jsx
@@ -1,30 +1,25 @@
 import style from "./SignLayout.module.scss"
 
-const SignLayout = ({children}) => 
-{
+const SignLayout = ({children}) => {
     return (
-        <div className={style.signLayout}>
-            <main>
-                <section className={style.formSection}>
+        <main className={style.signLayout}>
+            <section className={style.formSection}>
+                <div className={style.signLogo}>
+                    <img src="/img/maglo_logo.svg" alt="Maglo Logo" />
+                    <span>Maglo.</span>
+                </div>
+                <div className={style.signFormContainer}>
+                    {children}
+                </div>
+            </section>
+            <section className={style.imageSection}>
+                <div className={style.coverImage}>
                     <div>
-                        <div className={style.signLogo}>
-                            <img src="/img/maglo_logo.svg" alt="Maglo Logo" />
-                            <span>Maglo.</span>
-                        </div>
-                        <div className={style.signFormContainer}>
-                            {children}
-                        </div>
+                        <img src="/img/sign_image.png" alt="Sign Image" />
                     </div>
-                </section>
-                <section className={style.imageSection}>
-                    <div className={style.coverImage}>
-                        <div>
-                            <img src="/img/sign_image.png" alt="Sign Image" />
-                        </div>
-                    </div>
-                </section>
-            </main>
-        </div>
+                </div>
+            </section>
+        </main>
     )
 }
 
