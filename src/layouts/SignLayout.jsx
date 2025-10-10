adc59d36e195c0014a9d3ca279fb3bd16b0273d5
soztrk <s.ozturk999@gmail.com>
2025-10-10 14:44:48 +0300

    Sign layout for sign in and sign up

--- src/layouts/SignLayout.jsx
+++ src/layouts/SignLayout.jsx
@@ -3,10 +3,28 @@ import style from "./SignLayout.module.scss"
 const SignLayout = ({children}) => 
 {
     return (
-        <main className={style.signLayout}>
-            <img src="/img/maglo_logo.svg" alt="maglo logo" />
-            {children}
-        </main>
+        <div className={style.signLayout}>
+            <main>
+                <section className={style.formSection}>
+                    <div>
+                        <div className={style.signLogo}>
+                            <img src="/img/maglo_logo.svg" alt="Maglo Logo" />
+                            <span>Maglo.</span>
+                        </div>
+                        <div className={style.signFormContainer}>
+                            {children}
+                        </div>
+                    </div>
+                </section>
+                <section className={style.imageSection}>
+                    <div className={style.coverImage}>
+                        <div>
+                            <img src="/img/sign_image.png" alt="Sign Image" />
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </div>
     )
 }
 
