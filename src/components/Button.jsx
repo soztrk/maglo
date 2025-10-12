32d03d1f11d77dabf8dfa9472415c427a7851be0
soztrk <s.ozturk999@gmail.com>
2025-10-12 13:44:19 +0300

    Spinner component created and integrated to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,11 @@
 import style from "./Button.module.scss"
+import Spinner from "./Spinner"
 
 const Button = ({text,type,theme,icon,disabled,loading,onClick}) => {
 
     return (
-        <div className={[style.button, theme == "green" ? style.green : style.gray].join(" ")}>
-            <button type={type} onClick={onClick} disabled={disabled}>{loading?<span>O </span>:""}{icon}<span>{text}</span></button>
+        <div className={[style.button, theme == "green" ? style.green : ""].join(" ")}>
+            <button type={type} onClick={onClick} disabled={disabled}>{loading?<Spinner />:""}{icon}<span>{text}</span></button>
         </div>
     )
 }
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import style from "./Spinner.module.scss"
+
+const Spinner = () => {
+    return (
+        <span className={style.spinner}></span>
+    )
+}
+
+export default Spinner
